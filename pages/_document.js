8e7b03c7a5040d4669d747bb8d812d1e47e82cbf
jongwon <54282927+loosie@users.noreply.github.com>
2021-04-21 00:01:16 +0900

    metadata & footer update

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,14 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+
                     <link rel="icon" href="/favicon.ico" />
-                    {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossOrigin="anonymous" />
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossOrigin="anonymous" /> */}
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://richtest-moneyflow.netlify.app/" />
+                    <meta property="og:title" content="부를 쫓아라 - 돈의 흐름 읽기 테스트 (2021) " />
+                    <meta property="og:image" content="/bdot_logo2.png" />
+                    <meta property="og:description" content="21세기 문맹은 문자를 못 읽고 못 쓰는 사람이 아니라, 배우려 하지 않고 낡은 지식을 버리지 않고 재학습하지 않은 사람이다.- Alvin Toffler" />
+
                 </Head>
                 <body>
 
